bfd331da37989a576459d8e4457a4aaaee1e0e58
andreapanico12 <andreapanico12@gmail.com>
2025-04-10 10:42:19 +0200

    refactor debounce implementation and input handling for improved search efficiency

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
 import {useState, useEffect, useCallback} from 'react'
 
+/* 
+La funzione debounce limita la frequenza con cui una funzione può essere chiamata
+in un certo intervallo di tempo. In questo caso, limita la chiamata alla funzione
+di ricerca per evitare che venga chiamata ad ogni pressione di tasto
+La funzione debounce accetta una funzione di callback e un ritardo in millisecondi
+e restituisce una nuova funzione che può essere chiamata più volte, ma eseguirà
+la funzione di callback solo dopo che il ritardo è scaduto dall'ultima chiamata
+Questo è utile per evitare chiamate API eccessive mentre l'utente sta digitando
+Ad esempio, se l'utente digita "a", "b", "c" in rapida successione, la funzione
+di ricerca verrà chiamata solo una volta dopo che l'utente ha smesso di digitare
+per il tempo specificato dal ritardo*/
 
+//N.B. Debounce fuori dal componente!!!
+function debounce(callback, delay) {
+  let timer;
+
+  return (value) => {
+    clearTimeout(timer); // cancella il timeout precedente
+    timer = setTimeout(() => {
+      callback(value); // esegue la funzione solo dopo il delay
+    }, delay);
+  };
+}
 
 function App() {
 
@@ -8,17 +30,11 @@ function App() {
   const [query, setQuery] = useState('')
   const [suggestions, setSuggestions] = useState([])
 
-  function debounce(callback, delay) {
-    let timer;
-  
-    return (value) => {
-      clearTimeout(timer); // cancella il timeout precedente
-      timer = setTimeout(() => {
-        callback(value); // esegue la funzione solo dopo il delay
-      }, delay);
-    };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value)
+    handleSearch(e.target.value)
   }
-  
 
   const handleSearch = useCallback( debounce((newQuery) => {
     const fetchSuggestions = async () => {
@@ -30,11 +46,13 @@ function App() {
       const response = await fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/products?search=${newQuery}`)
       const data = await response.json()
       setSuggestions(data)
+      console.log(data)
       console.log(suggestions)
+  
 
     }
     fetchSuggestions();
-  }))
+  }, 500), [])
 
 
   
@@ -62,7 +80,7 @@ function App() {
     <>
     <h1 className='title'>AUTOCOMPLETE</h1>
     <div className='container'>
-      <input className= "searchbar"type="text" value={query} onChange={(e) => handleSearch(e.target.value)} />
+      <input className= "searchbar"type="text" value={query} onChange={handleInputChange} />
       {suggestions.length > 0 && (
         <ul className='suggestions'>
           {suggestions.map((suggestion) => (
